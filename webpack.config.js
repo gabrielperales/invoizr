a63ee77d925d0d1ab82326d6a4b69d569c038ecd
Gabriel Perales <gabrielperales@gmail.com>
2017-04-16 21:22:29 +0200

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+  },
+  devServer: {
+    inline: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [{
+      test: /\.elm$/,
+      exclude: [/elm-stuff/, /node_modules/],
+      loader: 'elm-webpack-loader' +
+        (process.env.NODE_ENV !== 'production' ? '?+debug' : ''),
+    }],
+  },
+  plugins: [
+  ],
+};
